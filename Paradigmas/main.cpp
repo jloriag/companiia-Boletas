a8fb389f0b3ed443e8371f58a72f64c9fd341c27
estudiante <estudiante@estudiante-HP-Compaq-Pro-6300-MT>
2014-10-23 09:33:21 -0600

    Commit Inicial Netbeans

---
+++ Paradigmas/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: estudiante
+ *
+ * Created on 23 de octubre de 2014, 09:32 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
